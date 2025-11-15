3588604f669ca897d8d10604bec21211cd403a39
Aleksey Berezka <alldmeat@icloud.com>
2025-11-16 00:31:23 +0500

    Upgrade to Swift 6.2 (#14)
    * Adopt Swift 6.2 toolchain
    
    * Remove sync helper and generalize docs
    
    * Revert doc changes
    
    * Enable Swift 6 language mode

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -28,5 +28,6 @@ let package = Package(
         .testTarget(
             name: "ADNetworkTests",
             dependencies: ["ADNetwork"]),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
